e75a77417b263c60ebc33a92c4f9a58365d581f6
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-09-27 16:50:32 -0300

    implemented ExibirFichaTecnica() and ExibirNomeEArtista() public methods

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -1,1 +1,26 @@
-﻿
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome:{nome}");
+        Console.WriteLine($"Artista:{artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine($"Disponível no plano");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+    }
+}
