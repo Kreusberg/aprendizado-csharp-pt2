96b9e063de1d55be182dcaed4cc6eb4c520b2fff
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-09-28 15:18:08 -0300

    Added constructor in Musica and Banda class

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,27 +1,35 @@
-﻿Album albumDoQueen = new Album();
-albumDoQueen.Nome = "A night at the opera";
+﻿Banda queen = new Banda("Queen");
+
+Album albumDoQueen = new Album("A night at the opera");
 
 //Genero genero = new Genero();
 //genero.Nome = "Rock";
 
-Musica musica1 = new Musica();
-musica1.Nome = "Love of my life";
-musica1.Duracao = 213;
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
 //musica1.Genero = new Genero();
 //musica1.Genero.Nome = "Rock";
 
-Musica musica2 = new Musica();
-musica2.Nome = "Bohemian Rhapsody";
-musica2.Duracao = 354;
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
 //musica2.Genero = new Genero();
 //musica2.Genero.Nome = "Rock";
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
-
-Banda queen = new Banda();
-queen.Nome = "Queen";
 queen.AdicionarAlbum(albumDoQueen);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumDoQueen.ExibirMusicasDoAlbum();
 queen.ExibirDiscografia();
 
 /*
